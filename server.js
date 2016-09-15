2178d16d2695e92533f17eeda7965094da7d0721
H3mann <mann.harp@gmail.com>
2016-09-15 14:36:22 -0400

    babel setup

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-import express from 'express';
+var express = require ('express');
+var bodyParser = require('body-parser');
+var router = require('express').Router();
+var path = require('path');
+var cors = require("cors");
 
+/* Initialize the server */
 let app = express();
+/* Add middleware */
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/", express.static(__dirname + "../client/"));
 
 app.get('/*', (req, res) => {
 	res.send('hello');
@@ -8,6 +18,7 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(1337) => {
-	console.log('listening to port')
+	console.log('listening to TEST!')
 }
 
+module.exports = app;
