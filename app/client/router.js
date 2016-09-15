d37e02b9e6be6328ed0601285f303e646dcf1e47
selimc <selimcebecioglu@gmail.com>
2016-09-15 19:37:23 -0400

    app.js

--- app/client/router.js
+++ app/client/router.js
@@ -3,15 +3,15 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
-import Test from './components/Test';
 import Test2 from './components/Test2';
+import App from './components/App';
 
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Test}>
+      <Route path="/" component={App}>
         <IndexRoute component={Test2}></IndexRoute>
-        <Route path="/test" component={Test}></Route>
+        <Route path="/test" component={Test2}></Route>
       </Route>
     </Router>
   </Provider>
