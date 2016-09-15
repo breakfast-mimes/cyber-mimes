2a93031bdb4b70083c2b7e555ef9f2bd45e486b0
selimc <selimcebecioglu@gmail.com>
2016-09-15 14:40:53 -0400

    webpack setup

--- app/server.js
+++ app/server.js
@@ -1,13 +1,27 @@
-import express from 'express';
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('../webpack.config');
 
-let app = express();
+var app = express();
+var compiler = webpack(config);
 
-app.get('/*', (req, res) => {
-	res.send('hello');
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-})
+app.use(require('webpack-hot-middleware')(compiler));
 
-app.listen(1337) => {
-	console.log('listening to port')
-}
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
+app.listen(1337, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:1337');
+});
