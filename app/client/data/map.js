8520eba7cc78c15b155147907851cebf7314bf3e
alexwade13 <alex.wade13@gmail.com>
2016-09-30 15:39:27 -0400

    story line

--- app/client/data/map.js
+++ app/client/data/map.js
@@ -4,7 +4,7 @@ const map = {
   images:[[ "http://bit.ly/2deaWuc","http://bit.ly/2d4k9lv", "http://bit.ly/2cGimRX",],
   ["http://bit.ly/2cUn5lV","http://bit.ly/2crqNnU", "http://bit.ly/2cGiroJ"],
   ["http://bit.ly/2dkDE8A", "http://bit.ly/2d4jS28", "http://bit.ly/2dcQUM4"],
-  ["https://hikemtshasta.files.wordpress.com/2013/04/cascades-plutos-cave-feb2013-047-copy-custom.jpg"]
+  ["https://hikemtshasta.files.wordpress.com/2013/04/cascades-plutos-cave-feb2013-047-copy-custom.jpg","http://bit.ly/2dcQUM4"]
   ],
 
   items: [[{name: "god-tier Sword", type: "meele", dmg: 12, stat: "str", equip: "rightHand"},{},{},{},{},{},{},{},{}],
@@ -23,6 +23,7 @@ const map = {
   [],
   ["spells", "inventory", "items"],
   []],
+
   soundEffects:[["","https://a.clyp.it/cn4un2pr.mp3"],
   [],
   [],
@@ -33,15 +34,37 @@ const map = {
 "[Selim]: We are still deciding what theme we want for our game, why don't you battle an enemy?[battlehere]"],
 
 ["Skip this page, possibly in the map, so that they cant move forward to it, but it sets them on the page after this after they battle on the previous page",
-"[Alex]: Sorry about that battle, our developer for the battle page was out celbrating his ~bIrThDaY~. [Harp]:Can you please do his job and find the missing features? He was drunk and left them laying around the map",
-"Cowboy map"],
+"Welcome! I'm fondly known as the Talking Mime! Pleased to you meet you. Do you like riddles? You better! Cause in order to finish the lazy programmers jobs, you need to solve riddles to get the features for the battle screen :D",
+"Howdy pardner, them high-fulutin software developers don't know how to decide on a theme! Why don't you click on that gun over there?[easter egg, picutre of gun that has yueh saying 'BANG BANG]'"]
 
 ["[Selim]: Hm, maybe we should add more features to battle? [Alex] THAT WAS YOUR JOB MAN! You can't just put all your work on the user... [Selim] Welp, too late, there in the map somewhere :D",
 "[Alex]: I needed an outlet for my bad jokes, I'm gonna add in a Jester character! [Harp]: Oh dear god, please no [Alex]: Too late (add Jester sound effect, alex talking)",
 "He who made me doesn't want me, he would buys me doesn't use me, he who use's me doesn't know it, what am I? [coffin]"//"[Harp]: Well I think the battle might be finished now! Go fight a goblin [Alex]: SPACE goblin! [Harp]: Shut up"
 ],
-[]]
+
+["This is a cave!!!!"]]
 
 }
+
+
+
+
+//   messages:[["",
+//   "[Harp]: Great news alert! I convinced the team to make this a fantasy game. The Sci Fi guys had me outnumbered, but like any hobbit knows, size doesn't matter!","[Alex]: Idk if Harp told you this was a fantasy game, but he's lying if he did. We're all about the scifi!",
+// "[Selim]: We are still deciding what theme we want for our game, why don't you battle an enemy?[battlehere]"],
+
+// ["Skip this page, possibly in the map, so that they cant move forward to it, but it sets them on the page after this after they battle on the previous page",
+// "[Alex]: Sorry about that battle, our developer for the battle page was out celbrating his ~bIrThDaY~. [Harp]:Can you please do his job and find the missing features? He was drunk and left them laying around the map",
+// "Cowboy map"],
+
+// ["[Selim]: Hm, maybe we should add more features to battle? [Alex] THAT WAS YOUR JOB MAN! You can't just put all your work on the user... [Selim] Welp, too late, there in the map somewhere :D",
+// "[Alex]: I needed an outlet for my bad jokes, I'm gonna add in a Jester character! [Harp]: Oh dear god, please no [Alex]: Too late (add Jester sound effect, alex talking)",
+// "He who made me doesn't want me, he would buys me doesn't use me, he who use's me doesn't know it, what am I? [coffin]"//"[Harp]: Well I think the battle might be finished now! Go fight a goblin [Alex]: SPACE goblin! [Harp]: Shut up"
+// ],
+
+// ["This is a cave!!!!"]]
+
+// }
+
 
 export default map;
