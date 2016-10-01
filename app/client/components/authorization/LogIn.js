86a28145fb36d6ebec77941512a2904679e2454b
selimc <selimcebecioglu@gmail.com>
2016-10-01 11:20:31 -0400

    style login and signup pages

--- app/client/components/authorization/LogIn.js
+++ app/client/components/authorization/LogIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Button, FormGroup, ControlLabel, FormControl, Form, HelpBlock, Col, PageHeader } from 'react-bootstrap';
 
 const LogIn = React.createClass({
   getInitialState() {
@@ -17,16 +18,83 @@ const LogIn = React.createClass({
     this.setState({pass: event.target.value});
   },
 
+  getValidationUsername() {
+    const length = this.state.name.length;
+    if (length > 0) return 'success';
+    else if (length > 20) return 'warning';
+    else if (length <= 0) return 'error';
+  },
+
+  getValidationPassword() {
+    const length = this.state.pass.length;
+    if (length > 0) return 'success';
+    else if (length > 20) return 'warning';
+    else if (length <= 0) return 'error';
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.login(this.state.name, this.state.pass);
+  },
+
+
   render() {
     return (
-      <div>
-        <div className='stats'>
-          <div className='statTitle'>USER LOGIN</div>
-          <input type="text" placeholder="username" onChange={this.updateName} className="statTitle"/>
-          <input type="password" placeholder="password" onChange={this.updatePass} className="statTitle"/>
-        </div>
-        <button type="submit" onClick={this.props.login.bind(null, this.state.name, this.state.pass)}>submit</button>
-      </div>
+      <Form horizontal>
+
+        <PageHeader>
+          <Col smOffset={2}>
+            Login <small>wanna play right?</small>
+          </Col>
+        </PageHeader>
+
+        <FormGroup
+          controlId="formUsername"
+          validationState={this.getValidationUsername()}>
+          <Col componentClass={ControlLabel} sm={2}>
+            Username
+          </Col>
+          <Col sm={2}>
+            <FormControl
+              type="text"
+              value={this.state.name}
+              placeholder="Username"
+              onChange={this.updateName} />
+            <FormControl.Feedback />
+          </Col>
+          <Col sm={2}>
+            <HelpBlock>Please enter a username.</HelpBlock>
+          </Col>
+        </FormGroup>
+
+        <FormGroup
+          controlId="formPassword"
+          validationState={this.getValidationPassword()}>
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={2}>
+            <FormControl
+              type="password"
+              value={this.state.pass}
+              placeholder="Password"
+              onChange={this.updatePass} />
+            <FormControl.Feedback />
+          </Col>
+          <Col sm={2}>
+            <HelpBlock>Please enter a password.</HelpBlock>
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button type="submit" onClick={this.handleSubmit}>
+              Login
+            </Button>
+          </Col>
+        </FormGroup>
+
+      </Form>
     )
   }
 });
