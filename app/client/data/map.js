1ff8e4f78a92dddddacff52cfcf840bd56c5efcc
H3mann <mann.harp@gmail.com>
2016-10-01 12:34:57 -0400

    dragon and sword fighters added to map

--- app/client/data/map.js
+++ app/client/data/map.js
@@ -27,8 +27,8 @@ const map = {
 
   easterEggs:[[{image:"http://i.imgur.com/5kyJyll.png", sound:"https://a.clyp.it/cn4un2pr.mp3"},""],
   [{image:"http://i.imgur.com/f2rt3Zy.png", sound:"https://a.clyp.it/mq5rd1d1.mp3"}],
-  [],
-  []],
+  [{image: "http://bestanimations.com/Fantasy/Dragons/dragon-animated-gif-40.gif"}],
+  [{image:'http://bestanimations.com/Fantasy/knight-fighting-diablo-deamon-animation.gif'}]],
 
   messages:[["",
   "My my, you don't look fancy enough to be in the steam punch genre! Take the corset, won't you?",
@@ -38,7 +38,7 @@ const map = {
 "Welcome! I'm fondly known as the Talking Mime! Pleased to meet you. Do you like riddles? You better! Cause in order to finish the lazy programmers jobs, you need to solve riddles to get the features for the battle screen :D",
 "Howdy pardner, them high-fulutin software developers don't know how to decide on a theme! Why don't you click on that gun over there?[easter egg, picutre of gun that has yueh saying 'BANG BANG]'"],
 
-["[Talking Mime] My army of Talking Mime's has set upon your castle! We all watched a Charlie Chaplin flick to drive us into a rage!! We are the voice for the voiceless!! I dare you to stop my attack.",
+["[Talking Mime] My army of Talking Mime's has set upon your castle! We all watched a Charlie Chaplin flick to drive us into a rage!! We are the voice for the voiceless!! I dare you to stop my attack. [Instruction] Find the best way to escape the attack on your castle and battle the talking mime!",
 "[Alex]: I needed an outlet for my bad jokes, I'm gonna add in a Jester character! [Harp]: Oh dear god, please no [Alex]: Too late (add Jester sound effect, alex talking)",
 ""
 ],
