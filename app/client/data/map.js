ff73c27fe1ff3346409a6c8fcf44f25932d6302c
H3mann <mann.harp@gmail.com>
2016-10-03 19:43:14 -0400

    css styling added

--- app/client/data/map.js
+++ app/client/data/map.js
@@ -1,12 +1,12 @@
 export const weapons = require('./items/weapons.json');
 
 const map = {
-  location: {row:5,col:0},
+  location: {row:1,col:1},
   // "http://bit.ly/2d4jS28"  http://bit.ly/2dcQUM4
   //http://wallpapercave.com/sci-fi-background
 
   images:[[ "http://wallpapercave.com/wp/565sgAQ.jpg","http://free.wallpaperbackgrounds.com/sci%20fi/steampunk/217481-43110.jpg", "http://bit.ly/2cGimRX"],
-  ["https://wallpaperscraft.com/image/canyon_desert_horseback_rider_wild_west_cowboy_45538_2048x2048.jpg","http://bit.ly/2crqNnU", "http://bit.ly/2cGiroJ"],
+  ["https://wallpaperscraft.com/image/canyon_desert_horseback_rider_wild_west_cowboy_45538_2048x2048.jpg","http://bit.ly/2dN8irq", "http://bit.ly/2cGiroJ"],
   ["http://bit.ly/2dxWrl2", "http://bit.ly/2d4jS28", "http://bit.ly/2dcQUM4"],
   ["https://hikemtshasta.files.wordpress.com/2013/04/cascades-plutos-cave-feb2013-047-copy-custom.jpg","http://bit.ly/2dcQUM4"],
   [],
